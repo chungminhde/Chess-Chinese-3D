acea432bd73a66bee9eebf3f0c965c1fbb4f726d
chungminhde <48831294+chungminhde@users.noreply.github.com>
2021-04-05 22:22:55 +0700

    Di chuyển của quân pháo.

--- Chess/Assets/Script/ChessPiece/BCannon.cs
+++ Chess/Assets/Script/ChessPiece/BCannon.cs
@@ -13,14 +13,28 @@ public class BCannon : Piece {
             for (int i = 1; i < 11; i++)
             {
                 Vector2Int nextGridPoint = new Vector2Int(gridPoint.x + i * dir.x, gridPoint.y + i * dir.y);
-                locations.Add(nextGridPoint);
+                if (!GameManager.instance.PieceAtGrid(nextGridPoint))
+                {
+                    locations.Add(nextGridPoint);
+                }
                 if (GameManager.instance.PieceAtGrid(nextGridPoint))
                 {
+                    for (int j = 1; j < 11 - i; j++)
+                    {
+                        Vector2Int nextGridPoint2 = new Vector2Int(gridPoint.x + (i + j) * dir.x, gridPoint.y + (i + j) * dir.y);
+                        if (GameManager.instance.PieceAtGrid(nextGridPoint2))
+                        {
+                            if (!GameManager.instance.FriendlyPieceAt(nextGridPoint2))
+                            {
+                                locations.Add(nextGridPoint2);
+                                break;
+                            }
+                        }
+                    }
                     break;
                 }
             }
         }
-
         return locations;
     }
 }
--- Chess/Assets/Script/ChessPiece/RCannon.cs
+++ Chess/Assets/Script/ChessPiece/RCannon.cs
@@ -13,19 +13,26 @@ public class RCannon : Piece {
             for (int i = 1; i < 11; i++)
             {
                 Vector2Int nextGridPoint = new Vector2Int(gridPoint.x + i * dir.x, gridPoint.y + i * dir.y);
+                if (!GameManager.instance.PieceAtGrid(nextGridPoint))
+                {
+                    locations.Add(nextGridPoint);
+                }
                 if (GameManager.instance.PieceAtGrid(nextGridPoint))
                 {
-                    for (int j = i; j < 11 - i; j++)
+                    for (int j = 1; j < 11 - i; j++)
                     {
-                        Vector2Int nextGridPoint2 = new Vector2Int(gridPoint.x + (i +j)* dir.x, gridPoint.y + (i +j)* dir.y);
+                        Vector2Int nextGridPoint2 = new Vector2Int(gridPoint.x + (i + j)* dir.x, gridPoint.y + (i + j)* dir.y);
                         if (GameManager.instance.PieceAtGrid(nextGridPoint2))
                         {
-                            locations.Add(nextGridPoint);
-                            break;
+                            if (!GameManager.instance.FriendlyPieceAt(nextGridPoint2))
+                            {
+                                locations.Add(nextGridPoint2);
+                                break;
+                            }
                         }
                     }
+                    break;
                 }
-                locations.Add(nextGridPoint);
             }
         }
         return locations;
